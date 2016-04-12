565edf7e7e7ca5032a882bba6715e629236419a2
Alejandro Garrido <alejandrogarrido@Alejandros-MacBook-Air.local>
2016-04-12 09:39:20 -0500

    working version

---
+++ public/JS/whackamole.js
@@ -0,0 +1,60 @@
+$(function() { 
+    var $square = $('.square');
+    var $start = ('#start');
+    var $title = $('#title');
+    var $timer = $('#timer');
+    var popUp = $square;
+    var i = 0;
+    var iconsArray = [];
+    var time = 50;
+
+    $($start).click(function() {
+        function togglePop() {
+            var random = parseInt(Math.random()*($square.length));
+            var randomPic = parseInt(Math.random()*6);
+            var popUp = $square[random];
+            $(popUp).addClass('pop');
+            $(popUp).css('background-image', 'url("/IMG/GoTicons'+[randomPic]+'.png")');
+            if ( i > 10) {
+                setTimeout(function() {
+                    $title.html("Level 2");
+                    $(popUp).removeClass('pop');
+                    $(popUp).css('background-image', '');
+                }, 700)
+            } else {
+                setTimeout(function() {
+                    $(popUp).removeClass('pop');
+                    $(popUp).css('background-image', '');
+                }, 1000)
+            }
+            $(popUp).click(function() {
+                if ($(this).hasClass('pop')) {
+                    i++;
+                    time++;
+                    $('#score').html("Score: " + i)
+                    setTimeout(function() {
+                        $timer.html("Time: " + time + "+1")
+                    }, 300)                    
+                }
+                $(this).removeClass('pop');
+                $(this).css('background-image', '');
+            })
+        }
+        var gameTime = setInterval(togglePop, 2000);
+        var gameOver = setInterval(function() {
+            $timer.html("Time: " + time)
+            if (time > 0) {
+                time -= 1;
+            } else {
+                clearInterval(gameTime);
+                clearInterval(gameOver);
+                $title.html("Game Over");
+                setTimeout(function() {
+                    $title.html("Whack-A-Something");
+                    time = 30;
+                    score = 0;
+                }, 2000)
+            }
+        }, 1000)
+    })
+});
